fc505479cf2ff43eb71d2fcf233cb9a8aebd2a9f
shivam.patidar <shivam.patidar@deeporion.com>
2023-04-03 12:06:58 +0530

    Create a UI to create new posts - Inside the PostsController:   define new and create actions   define a post_params method   define a before_action filter - Inside the ApplicationController:   define a redirect_if_not_signed_in method   define a redirect_if_signed_in method - Create a new template for posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,9 @@
 class PagesController < ApplicationController
 
     def index
-        def index
-            @hobby_posts = Post.by_branch('hobby').limit(8)
-            @study_posts = Post.by_branch('study').limit(8)
-            @team_posts = Post.by_branch('team').limit(8)
-          end
+        @hobby_posts = Post.by_branch('hobby').limit(8)
+        @study_posts = Post.by_branch('study').limit(8)
+        @team_posts = Post.by_branch('team').limit(8)
     end
+    
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,45 +1,59 @@
 class PostsController < ApplicationController
 
-    def show
-        @post = Post.find(params[:id])
-    end
+  before_action :redirect_if_not_signed_in, only: [:new]
 
-    def hobby
-        posts_for_branch(params[:action])
-    end
-    
-    def study
-        posts_for_branch(params[:action])
-    end
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
 
-    def team
-        posts_for_branch(params[:action])
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to post_path(@post) 
+    else
+      redirect_to root_path
     end
+  end
 
-    private
-    def posts_for_branch(branch)
-        @categories = Category.where(branch: branch)
-        @posts = get_posts.paginate(page: params[:page])
-        respond_to do |format|
-          format.html
-          format.js { render partial: 'posts/posts_pagination_page' }
-        end
-    end
+  def show
+      @post = Post.find(params[:id])
+  end
 
-    def get_posts
-        branch = params[:action]
-        search = params[:search]
-        category = params[:category]
-      
-        if category.blank? && search.blank?
-          posts = Post.by_branch(branch).all
-        elsif category.blank? && search.present?
-          posts = Post.by_branch(branch).search(search)
-        elsif category.present? && search.blank?
-          posts = Post.by_category(branch, category)
-        elsif category.present? && search.present?
-          posts = Post.by_category(branch, category).search(search)
-        else
-        end
+  def hobby
+      posts_for_branch(params[:action])
+  end
+  
+  def study
+      posts_for_branch(params[:action])
+  end
+
+  def team
+      posts_for_branch(params[:action])
+  end
+
+  private
+  def posts_for_branch(branch)
+      @categories = Category.where(branch: branch)
+      @posts = get_posts.paginate(page: params[:page])
+      respond_to do |format|
+        format.html
+        format.js { render partial: 'posts/posts_pagination_page' }
       end
+  end
+
+  def get_posts
+    PostsForBranchService.new({
+      search: params[:search],
+      category: params[:category],
+      branch: params[:action]
+    }).call
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :title, :category_id)
+                         .merge(user_id: current_user.id)
+  end
+  
 end
