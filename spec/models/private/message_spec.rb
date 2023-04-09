8f80bb339514b2962872ffa597e377603b1bb2ff
shivam.patidar <shivam.patidar@deeporion.com>
2023-04-09 14:32:53 +0530

    Broadcast a new message - Inside the Private::Message define an after_create_comit callback method. - Create a Private::MessageBroadcastJob - Define a received function inside the   assets/javascripts/channels/private/conversation.js - Create a conversation.js inside the   assets/javascripts/channels/shared

--- spec/models/private/message_spec.rb
+++ spec/models/private/message_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Private::Message, type: :model do
+    context 'Methods' do
+        it 'gets a previous message' do
+          conversation = create(:private_conversation)
+          message1 = create(:private_message, conversation_id: conversation.id)
+          message2 = create(:private_message, conversation_id: conversation.id)
+          expect(message2.previous_message).to eq message1
+        end
+      end
 end
